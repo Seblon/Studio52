757cd9d6272f3346471690cf19e65340f38b00bf
Sebbe <jsbmedia@gmail.com>
2020-01-09 12:48:12 +0100

    Completed the build process implemenation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
+
+class RunAfterCompile {
+   apply(compiler) {
+      compiler.hooks.done.tap('Copy images', function () {
+         fse.copySync('./app/assets/img', './dist/assets/img')
+      })
+   }
+}
+
 const postCSSPlugins = [
    require('postcss-import'),
    require('postcss-mixins'),
@@ -7,13 +21,37 @@ const postCSSPlugins = [
    require('autoprefixer')
 ]
 
-module.exports = {
+let cssConfig = {
+   test: /\.css$/i,
+   use: ['css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }]
+}
+
+let pages = fse.readdirSync('./app').filter(function (file) {
+   return file.endsWith('.html')
+}).map(function (page) {
+   return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`
+   })
+})
+
+let config = {
    entry: './app/assets/scripts/App.js',
-   output: {
+   plugins: pages,
+   module: {
+      rules: [
+         cssConfig
+      ]
+   }
+}
+
+if (currentTask == 'dev') {
+   cssConfig.use.unshift('style-loader')
+   config.output = {
       filename: 'bundeled.js',
       path: path.resolve(__dirname, 'app')
-   },
-   devServer: {
+   }
+   config.devServer = {
       before: function (app, server) {
          server._watch('./app/**/*.html')
       },
@@ -22,14 +60,26 @@ module.exports = {
       port: 3000,
       host: '0.0.0.0'
       // Localhost för andra enheter: 192.168.72.42:3000
-   },
-   mode: 'development',
-   module: {
-      rules: [
-         {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }]
-         }
-      ]
    }
+   config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+   cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+   config.output = {
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'dist')
+   }
+   config.mode = 'production'
+   config.optimization = {
+      splitChunks: { chunks: 'all' }
+   }
+   config.plugins.push(
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
+      new RunAfterCompile()
+   )
+}
+
+module.exports = config
